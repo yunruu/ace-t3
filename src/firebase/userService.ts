5737f90be70e35834a851ab582dffd2d334eb85f
yunruu <yunruuchin@gmail.com>
2025-04-07 02:46:46 +0800

    feat: register user with uuid

--- src/firebase/userService.ts
+++ src/firebase/userService.ts
@@ -1,10 +1,15 @@
 import bcrypt from "bcryptjs";
 import { db } from "./config";
 import { doc, getDoc, setDoc } from "firebase/firestore";
+import { User } from "@/types";
+import { getRandUuid } from "./service";
 
 const USERS_COLLECTION = "users";
 
-export const registerUser = async (username: string, password: string) => {
+export const registerUser = async (
+  username: string,
+  password: string,
+): Promise<User> => {
   const userRef = doc(db, USERS_COLLECTION, username);
   const existingUser = await getDoc(userRef);
 
@@ -12,18 +17,22 @@ export const registerUser = async (username: string, password: string) => {
     throw new Error("Username already exists");
   }
 
+  const uuid = getRandUuid();
   const hashedPassword = await bcrypt.hash(password, 8);
-
   await setDoc(userRef, {
+    id: uuid,
     username,
     password: hashedPassword,
     createdAt: new Date().toISOString(),
   });
 
-  return { username };
+  return new User(uuid, username);
 };
 
-export const loginUser = async (username: string, password: string) => {
+export const loginUser = async (
+  username: string,
+  password: string,
+): Promise<User> => {
   const userRef = doc(db, USERS_COLLECTION, username);
   const userSnap = await getDoc(userRef);
 
@@ -38,5 +47,5 @@ export const loginUser = async (username: string, password: string) => {
     throw new Error("Incorrect password");
   }
 
-  return { username };
+  return userData as User;
 };
